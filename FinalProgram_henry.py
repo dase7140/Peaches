c5e7bfa0e00b814728144c5d899f43ef690a6633
Henry Stevens <henry.stevens@colorado.edu>
2025-11-26 15:26:27 -0700

    added decision trees for most modes, need to implement bridge mode still, will need camera modules and probably testing

--- FinalProgram_henry.py
+++ FinalProgram_henry.py
@@ -80,6 +80,7 @@ robot_state = {
 	# Pixy camera block detection
       "pixy_block_count": 0,
       "pixy_blocks": [],
+	  "dice_visible": False,
 
 	# Pixy camera flag for gravel/ramp detection
 	  "gravel_flag_detected": False,
@@ -107,6 +108,13 @@ robot_state = {
 
 	# Variables calculated from sensor data
       "front_obstacle_distance": -1,
+
+	# Timing variables used in modes
+	  "turn_start_time" : None,
+	  "reverse_start_time" : None,
+	  "bridge_start_time" : None,
+	  "bridge_aligned": False,
+	  
 }
 
 def command_robot(direction, angle, speed, tray_position, brush_speed):
@@ -122,7 +130,7 @@ def command_robot(direction, angle, speed, tray_position, brush_speed):
 	if direction not in ['L','R','B']:
 		raise ValueError("Invalid direction")
 	
-	if isinstance(angle, int):
+	if isinstance(angle, float):
 		if not (0 <= angle <= 99):
 			raise ValueError("Invalid angle")
 		else:
@@ -159,49 +167,49 @@ def command_robot(direction, angle, speed, tray_position, brush_speed):
 	arduino.write(command.encode())
 
 # sepatarate thread to read arduino sensor data, parses and updates robot_state. Other messages such as errors should be parsed here as well.
-def read_arduino_thread(ser):
-	"""
-	Thread: continuously read lines from Arduino, parse SENS packets.
-	Expected format:
-		SENS USF_L=123 USF_R=128 USS=87 USL=210 USR=199 IRF_L=1 IRF_R=0 IRS=0 IRL=1 IRR=0
-	If a value cannot be parsed, put -1 for that key.
-	"""
-	while True:
-		try:
-			line = ser.readline().decode(errors="ignore").strip()
-			if not line.startswith("SENS"):
-				continue
+# def read_arduino_thread(ser):
+# 	"""
+# 	Thread: continuously read lines from Arduino, parse SENS packets.
+# 	Expected format:
+# 		SENS USF_L=123 USF_R=128 USS=87 USL=210 USR=199 IRF_L=1 IRF_R=0 IRS=0 IRL=1 IRR=0
+# 	If a value cannot be parsed, put -1 for that key.
+# 	"""
+# 	while True:
+# 		try:
+# 			line = ser.readline().decode(errors="ignore").strip()
+# 			if not line.startswith("SENS"):
+# 				continue
 
-			parts = line.split()
-			parsed = {}
+# 			parts = line.split()
+# 			parsed = {}
 
-			for p in parts[1:]:
-				if "=" in p:
-					key, val = p.split("=")
-					try:
-						parsed[key] = int(val)
-					except:
-						parsed[key] = -1
+# 			for p in parts[1:]:
+# 				if "=" in p:
+# 					key, val = p.split("=")
+# 					try:
+# 						parsed[key] = int(val)
+# 					except:
+# 						parsed[key] = -1
 
-			with state_lock:
-				# Update only the keys that exist in robot_state
-				for key in robot_state["sensors"]:
-					if key in parsed:
-						robot_state["sensors"][key] = parsed[key]
+# 			with state_lock:
+# 				# Update only the keys that exist in robot_state
+# 				for key in robot_state["sensors"]:
+# 					if key in parsed:
+# 						robot_state["sensors"][key] = parsed[key]
 
-				robot_state["last_arduino_comm"] = time.time()
+# 				robot_state["last_arduino_comm"] = time.time()
 
-		except Exception as e:
-			print("Arduino read error:", e)
-			time.sleep(0.01)
+# 		except Exception as e:
+# 			print("Arduino read error:", e)
+# 			time.sleep(0.01)
 
-# Start Arduino reading thread
-arduino_thread = threading.Thread(
-	target=read_arduino_thread, 
-	args=(arduino,),
-	daemon=True
-)
-arduino_thread.start()
+# # Start Arduino reading thread
+# arduino_thread = threading.Thread(
+# 	target=read_arduino_thread, 
+# 	args=(arduino,),
+# 	daemon=True
+# )
+# arduino_thread.start()
 
 # Thread to read Pixy blocks, update robot_state - Need to ask dania about finishing this and adding relevent variables to robot_state
 def read_pixy_thread():
@@ -240,6 +248,9 @@ def calculate_centering(USL, USR,IRL, IRR):
 	ultrasonic_error = USL - USR if USL >=0 and USR >=0 else 0
 	ir_error = IRL - IRR if IRL >=0 and IRR >=0 else 0
 	
+	if abs(ultrasonic_error) > 100 and abs(ir_error) > 100: #implies open wall, on side, don't adjust TODO test bounds
+		return 0
+
 	# Weighted sum (weights can be adjusted)
 	total_error = (ultrasonic_error * 0.5) + (ir_error * 0.5) # need to calibrate this weighting so we don't overcorrect; should return 
 	if abs(total_error) > 99:  # deadband threshold
@@ -288,16 +299,158 @@ def go_tree(current_state):
 	"""
 	# This function will be a series of checks to update robot_state["mode"] based on sensor and camera data. 
 	# Its basically a decision tree to figure out what to do next, will need to play around to figure out priorities;
-	# if we are far 
+	# The hierarchy of decisions can be seen in the documentation
+	wallDistance = current_state["front_obstacle_distance"] 
+	if wallDistance >= 0 and wallDistance < 15:
+		# Obstacle detected ahead, decide turn direction
+		USL = current_state["sensors"]["USL"]
+		IRL = current_state["sensors"]["IRL"]
+		leftDistance = IRL if IRL >= 0 else USL
+		USR = current_state["sensors"]["USR"]
+		IRR = current_state["sensors"]["IRR"]
+		rightDistance = IRR if IRR >= 0 else USR
+		# Decide turn direction based on more open side
+		if leftDistance >= 25 and leftDistance > rightDistance:
+			return "turn_left"
+		elif rightDistance >= 25 and rightDistance > leftDistance:
+			return "turn_right"	
+		else: # both sides blocked, reverse
+			return "reverse"
+	else:
+		dice_visible = current_state["dice_visible"]
+		if dice_visible:
+			return "dice_track"
+		else:
+			return "go"
+
+def turn_left_tree(current_state):
+	"""
+	Implements logic to update robot mode from "turn_left" state.
+	"""
+	# This function will be a series of checks to update robot_state["mode"] based on sensor and camera data. 
+	# Its basically a decision tree to figure out what to do next, will need to play around to figure out priorities;
+	# The hierarchy of decisions can be seen in the documentation
+	if current_state["turn_start_time"] is None:
+		with state_lock:
+			robot_state["turn_start_time"] = time.time()
+	
+	else :
+		turn_duration = time.time() - current_state["turn_start_time"] # if we have been turning for less than threshold time, keep turning
+		if turn_duration > 3.0:
+			with state_lock:
+				robot_state["turn_start_time"] = None
+			return "go"
+		if turn_duration < 0.5: #TODO dial in time threshold
+			return "turn_left"	
+		else: # check front sensors to see if we are clear to go forward again
+			USFL = current_state["sensors"]["USFL"]
+			USFR = current_state["sensors"]["USFR"]
+			IRFL = current_state["sensors"]["IRFL"]
+			IRFR = current_state["sensors"]["IRFR"]
+			frontDistance = calculate_obstacle_distance(USFL, USFR, IRFL, IRFR)
+
+			gravelFlag = current_state["gravel_flag_detected"]
+			rampFlag = current_state["ramp_flag_detected"]
+		if frontDistance >= 20:  # TODO dial in distance threshold
+			if gravelFlag: # once we see gravel, this flag will be set in the robot_state object by the pixy thread. We enter gravel mode if we've seen that flag, and the front is clear again. We reset the flag once we exit gravel mode
+				with state_lock:
+					robot_state["turn_start_time"] = None
+				return "gravel"
+			elif rampFlag:
+				with state_lock:
+					robot_state["turn_start_time"] = None
+					robot_state["bridge_start_time"] = time.time()
+				return "bridge" # bridge mode is gonna be a complicated one, we enter it coming out of this turn, but it will need to go forwards a bit, align with the bridge, and then cross it. This will require testing!!
+			else: 
+				with state_lock:
+					robot_state["turn_start_time"] = None
+				return "go"
+		else:
+			return "turn_left"
+
+def turn_right_tree(current_state):
+	"""
+	Implements logic to update robot mode from "turn_right" state.
+	"""
+	# This function will be a series of checks to update robot_state["mode"] based on sensor and camera data. 
+	# Its basically a decision tree to figure out what to do next, will need to play around to figure out priorities;
+	# The hierarchy of decisions can be seen in the documentation
+	if current_state["turn_start_time"] is None:
+		with state_lock:
+			robot_state["turn_start_time"] = time.time()
+	
+	else :
+		turn_duration = time.time() - current_state["turn_start_time"] # if we have been turning for less than threshold time, keep turning
+		if turn_duration > 3.0:
+			with state_lock:
+				robot_state["turn_start_time"] = None
+			return "go"
+		if turn_duration < 0.5: #TODO dial in time threshold
+			return "turn_right"	
+		else: # check front sensors to see if we are clear to go forward again
+			USFL = current_state["sensors"]["USFL"]
+			USFR = current_state["sensors"]["USFR"]
+			IRFL = current_state["sensors"]["IRFL"]
+			IRFR = current_state["sensors"]["IRFR"]
+			frontDistance = calculate_obstacle_distance(USFL, USFR, IRFL, IRFR)
+
+			gravelFlag = current_state["gravel_flag_detected"]
+			rampFlag = current_state["ramp_flag_detected"]
+		if frontDistance >= 20:  # TODO dial in distance threshold
+			if gravelFlag: # once we see gravel, this flag will be set in the robot_state object by the pixy thread. We enter gravel mode if we've seen that flag, and the front is clear again. We reset the flag once we exit gravel mode
+				with state_lock:
+					robot_state["turn_start_time"] = None
+				return "gravel"
+			elif rampFlag:
+				with state_lock:
+					robot_state["turn_start_time"] = None
+				return "bridge" # bridge mode is gonna be a complicated one, we enter it coming out of this turn, but it will need to go forwards a bit, align with the bridge, and then cross it. This will require testing!!
+			else: 
+				with state_lock:
+					robot_state["turn_start_time"] = None
+				return "go"
+		else:
+			return "turn_right"
+
+def reverse_tree(current_state):
+	if current_state["reverse_start_time"] is None:
+		with state_lock:
+			robot_state["reverse_start_time"] = time.time()
+	else:
+		reverse_duration = time.time() - current_state["reverse_start_time"]
+		if reverse_duration > 1.0: # reverse for 1 seconds
+			with state_lock:
+				robot_state["reverse_start_time"] = None
+			return "go"
+		else:
+			return "reverse"
+
+def gravel_tree(current_state):
+	"""
+	Implements logic to update robot mode from "gravel" state.
+	"""
+	# This function will be a series of checks to update robot_state["mode"] based on sensor and camera data. 
+	# Its basically a decision tree to figure out what to do next, will need to play around to figure out priorities;
+	# The hierarchy of decisions can be seen in the documentation
+	wallDistance = current_state["front_obstacle_distance"] 
+	if wallDistance >= 0 and wallDistance < 15:
+		# Obstacle detected ahead, decide turn direction
+		return "turn_left"  # for now just always turn left out of gravel, can improve later
+	else:
+		return "gravel"
+
+def bridge_tree(current_state):
+	bridge_duration = time.time() - current_state["bridge_start_time"]
+
 
 def main_loop():
 	"""
 	Main loop: decide robot mode based on sensor data in robot_state.
 	"""
+	# while True:
+	# 	command_robot('L', 0, 99, 'D', 0)  # stop robot initially
+	# 	time.sleep(1)  # wait a second before starting main loop
 	while True:
-		command_robot('L', 0, 99, 'D', 0)  # stop robot initially
-		time.sleep(1)  # wait a second before starting main loop
-	while False:
 		with state_lock:
 			# Copy current state for decision making
 			current_state = robot_state.copy()
@@ -305,12 +458,14 @@ def main_loop():
 		# Decision logic based on current_state
 		# Example:
 		mode = current_state["mode"]
+		next_mode = mode  # default to current mode
 		sensors = current_state["sensors"]
 		pixy_blocks = current_state["pixy_blocks"]
 		yellow_detected = current_state["yellow_detected"]
-
+		print(f"Current mode: {mode}")
 		front_obstacle_distance = calculate_obstacle_distance(
 			sensors["USFL"], sensors["USFR"],sensors["IRFL"], sensors["IRFR"])
+		current_state["front_obstacle_distance"] = front_obstacle_distance
 
 		if mode == "go": # centers robot on track and moves forward, slows down if appraching obstacles
 			angle_adjust = calculate_centering(
@@ -321,31 +476,61 @@ def main_loop():
 				front_obstacle_distance
 			)
 			command_robot(
-				'L' if angle_adjust > 0 else 'R' if angle_adjust < 0 else 'B',
-				abs(angle_adjust) if angle_adjust != 0 else 'XX',
+				'L' if angle_adjust >= 0 else 'R' if angle_adjust < 0 else 'B',
+				abs(angle_adjust),
 				speed_ahead,
 				'D',
 				5  # example brush speed
 			)
-			go_tree(robot_state)
+			next_mode = go_tree(robot_state)
 		
-		elif mode == "turn_left":
-			pass
+		elif mode == "turn_left": # implements turn left logic; starts turning and records time, exit conditions are when a certain time has passed and the left  and front sensors reads a far distance again (will need to calibrate time)
+			#behavior in this mode is just turning in place, dice collection system can continue
+			command_robot("L","XX",50,'D',5)
+			next_mode = turn_left_tree(robot_state)
+
 		elif mode == "turn_right":
-			pass
-		elif mode == "reverse":
-			pass
-		elif mode == "stop":
-			pass
-		elif mode == "gravel":
-			pass
-		elif mode == "bridge":
-			pass
+			command_robot("R","XX",50,'D',5)
+			next_mode = turn_right_tree(robot_state)
+
+		elif mode == "reverse": # for now just moves backwards for a set time (1 second), then goes to go mode. Will need to add logic to determine when to exit reverse based on sensor readings
+			command_robot("B","00",50,'D',0)
+			next_mode = reverse_tree(robot_state)
+
+		elif mode == "stop":  # just stops all motors, breaks the loop to end the program. We can have some trigger for this if we want, but otherwise its unused 
+			command_robot('L', 'XX', 0, 'D', 0)
+			next_mode = "stop"
+			break
+		elif mode == "gravel": # this mode is entered once we see the gravel flag after a turn, we need to move forward and continue alignment
+			# similar to go but with tray raised. Can implement some protocol to get brush out of way, like briefly running brush in reverse, or just ball
+			angle_adjust = calculate_centering(
+				sensors["USL"], sensors["USR"],
+				sensors["IRL"], sensors["IRR"]
+			)
+			speed_ahead = calculate_speed_ahead(
+				front_obstacle_distance
+			)
+			command_robot(
+				'L' if angle_adjust >= 0 else 'R' if angle_adjust < 0 else 'B',
+				abs(angle_adjust),
+				speed_ahead,
+				'U',
+			  	0)  # example brush speed
+			next_mode = gravel_tree(robot_state)
+			
+		elif mode == "bridge": # bridge mode is gonna be a complicated one, we enter it coming out of this turn, but it will need to go forwards a bit, align with the bridge, and then cross it. This will require testing!!
+			# implement with cameras, etc. will probably be histeresis based, where we have a series of steps to align with and cross the bridge
+			# not sure how much of this can be done with the camera, we could align usung the two sensors on the front (go until both front sensors read equal distance, then cross bridge for set time)
+			# TODO implement bridge logic
+			next_mode = bridge_tree(robot_state)
+			
 		elif mode == "dice_track":
+			# TODO this is a variation on Go where it picks whichever dice is most cenetered and aims for that.
+			# this will need to use pixy blocks to determine dice positions. Will need some threshold to preven getting too close to walls, we can't get dice that're within ~3" of walls
 			pass
 		
 		with state_lock:
 			# Update robot_state if needed
-			robot_state["mode"] = mode  # may need to update more parameters, but for now just sets mode for next loop iteration
-
+			robot_state["mode"] = next_mode  # may need to update more parameters, but for now just sets mode for next loop iteration
+			robot_state["front_obstacle_distance"] = front_obstacle_distance
 		time.sleep(0.1)  # Adjust loop rate as needed
